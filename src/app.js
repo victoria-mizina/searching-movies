a814a5a706983f0f74da5d466ff797878350e977
victoria-mizina <vicmizina@yandex.ru>
2026-07-18 20:00:24 +0400

    added header and form for serching movies

---
+++ src/app.js
@@ -0,0 +1,12 @@
+function app(){
+    console.log(1)
+    const stateInit = {
+        movies: [],
+        error: ''
+    }
+    const elements = {
+        button: document.querySelector('.input>button'),
+        input: document.querySelector('.search')
+    }
+}
+app()
