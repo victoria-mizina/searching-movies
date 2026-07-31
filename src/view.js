608d2f38a8eed8fdb30d28cf960dc5ab2e8cd2c2
victoria-mizina <vicmizina@yandex.ru>
2026-07-31 22:00:34 +0400

    render movies

--- src/view.js
+++ src/view.js
@@ -8,7 +8,25 @@ function view (state,elements){
     return watch
 }
 function renderMovies (movies){
-    const div = document.querySelector('.movies')
-    
+    const divMovies = document.querySelector('.movies')
+    divMovies.innerHTML = ''
+    for(const movie of movies){
+        const title = document.createElement('h1')
+        const type = document.createElement('p')
+        const year = document.createElement('p')
+        const poster = document.createElement('img')
+        const divMovie = document.createElement('div')
+
+        title.textContent = movie.Title
+        type.textContent = movie.Type
+        year.textContent = movie.Year
+        poster.src = movie.Poster
+
+        divMovie.append(poster,title,type,year)
+        divMovies.append(divMovie)
+
+
+
+    }
 }
 export default view
